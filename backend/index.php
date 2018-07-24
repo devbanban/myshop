c885786afdd06d1cc536b59afda31778de9d0979
devbanban <devbanban@gmail.com>
2018-07-24 12:26:16 +0700

    backend

--- backend/index.php
+++ backend/index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php include('h.php');?>
+    <?php include('datatable.php');?>
     
   </head>
   <body>
